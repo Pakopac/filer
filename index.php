1e38109f41666e9b6b05cb9c37cb9c01dbc7927c
unknown <lilian.pacaud@supinternet.fr>
2017-11-30 20:07:28 +0100

    Added header

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <div class="titles">
+            <h1>hyperFiles</h1>
+            <h2>upload and share</h2>
+            </div>
+            <div class="login">
+                login
+                <br> pseudo: <input type="text">
+                <br>password: <input type="password">
+                <br> <button>login</button>
+            </div>
+        </header>
+    </body>
+</html>
