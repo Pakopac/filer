dca2d96784e3baaaa5adfef22ce24681099e0b8f
Lilian Pacaud <lilian.pacaud@supinternet.fr>
2017-12-28 22:26:50 +0100

    Added message for users not logged in

--- index.php
+++ index.php
@@ -4,7 +4,15 @@ require('init.php');
 $title = "Home";
 
 ob_start();
+?>
+ <?php if (!isset($_SESSION['pseudo'])): ?>
+    <link rel="stylesheet" href="styles.css">
+    <div class="messageWelcome">Welcome to this site</div>
+        <div class="messageNotLogged">You need to logged in to upload and access at your files</div>
+ <?php endif; ?>
+<?php
 $content = ob_get_contents();
 ob_end_clean();
 
 require('layout.php');
+
