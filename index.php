48ea07481421736bf05110e449dd5d570b8df72e
Lilian Pacaud <lilian.pacaud@supinternet.fr>
2017-12-28 22:40:36 +0100

    Added message for users logged and input file

--- index.php
+++ index.php
@@ -5,11 +5,15 @@ $title = "Home";
 
 ob_start();
 ?>
- <?php if (!isset($_SESSION['pseudo'])): ?>
     <link rel="stylesheet" href="styles.css">
+ <?php if (!isset($_SESSION['pseudo'])): ?>
     <div class="messageWelcome">Welcome to this site</div>
-        <div class="messageNotLogged">You need to logged in to upload and access at your files</div>
+    <div class="messageNotLogged">You need to logged in to upload and access at your files</div>
  <?php endif; ?>
+<?php if (isset($_SESSION['pseudo'])):?>
+    <div class="messageLogged">Upload and share your files here :</div>
+    <input type="file">
+<?php endif; ?>
 <?php
 $content = ob_get_contents();
 ob_end_clean();
