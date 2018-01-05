db0e642b5bed6b7f8da0986a6163c020c2883030
Lilian Pacaud <lilian.pacaud@supinternet.fr>
2018-01-05 22:35:53 +0100

    List Files upload in checkFiles.php

--- checkFiles.php
+++ checkFiles.php
@@ -6,6 +6,16 @@ ob_start();
 ?>
     <div class="link"><a href="uploadFiles.php">Click here to upload a file</a></div>
 <?php
+echo '<ul>';
+
+if($dir = opendir('./files')) {
+    while (false !== ($file = readdir($dir))) {
+        if($file != '.' && $file != '..' && $file != 'index.php') {
+            echo '<li><a class="files" href="./files/' . $file . '">' . $file . '</a></li>';
+        }
+        echo '</ul>';
+    }
+}
 $content = ob_get_contents();
 ob_end_clean();
 require ('layout.php');
