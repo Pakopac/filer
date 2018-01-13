6779bad22376450e5523defb4f7a31925e62ad26
Lilian Pacaud <lilian.pacaud@supinternet.fr>
2018-01-13 18:03:56 +0100

    create systeme for rename

--- checkFiles.php
+++ checkFiles.php
@@ -1,8 +1,13 @@
-<script src="script.js"></script>
 <?php
 require ('init.php');
 $title = "MyFiles";
 $pseudo = ($_SESSION['pseudo']);
+if (isset($_POST['newName'])) {
+    $rename = $_POST['newName'] ;
+};
+if (isset($_GET['name'])){
+    $fileName = $_GET['name'];
+    };
 
 ob_start();
 ?>
@@ -10,19 +15,27 @@ ob_start();
     <ul>
 <?php if ($dir = opendir('files/' . $pseudo . '')): ?>
     <?php while (false !== ($file = readdir($dir))): ?>
-        <?php if ($file != '.' && $file != '..'): ?>
+        <?php if ($file != '.' && $file != '..'):
+            ?>
             <li class="file"><a name="nameFile" class="nameFile" href="files/<?= $pseudo ?>/<?= $file?>" download><?= $file ?></a>
-                <a href="#pop-up"><img class="icon_edit" src="img/icon_edit.png"></a>
+                <a href="?name=<?= $file?>#pop-up"><img class="icon_edit" src="img/icon_edit.png"></a>
                 <a href=" delete.php?name=<?= $file ?>"> <img class="icon_delete" src="img/icon_delete.png"></a>
             </li>
             <div id="pop-up">
                 <div class="popUp">
-                <ul>
-                    <li class="exitPopup"><a href="#">X</a></li>
-                    <li class="edit">Change the File name</li>
-                    <li><input class="inputEdit" type="text" value="<?= $file?>"></li>
-                    <li><a href="#"><button class="btnForm" id="btnEdit">Enter</button></a></li>
-                </ul>
+                    <ul>
+                        <form method="POST">
+                            <li class="exitPopup"><a href="#">X</a></li>
+                            <li class="edit">Change the File name</li>
+                            <li><input name="newName" class="inputEdit" type="text" value=<?=$fileName?>></li>
+                            <li><a href="#"><button class="btnForm" id="btnEdit">Enter</button></a></li>
+                            <?php if (isset($rename)){
+                                $file = $fileName;
+                                header("Location: rename.php?name=$file&newName=$rename");
+                                }
+                                ?>
+                        </form>
+                    </ul>
                 </div>
             </div>
         <?php endif; ?>
--- rename.php
+++ rename.php
@@ -3,5 +3,6 @@ require ('init.php');
 $file = $_GET['name'];
 $pseudo = ($_SESSION['pseudo']);
 $dir = 'files/' . $pseudo . '/';
-unlink($dir.$file);
-header('Location: checkFiles.php');
+$newName = $_GET['newName'];
+rename($dir.$file, $dir.$newName);
+header('Location: checkFiles.php#');
